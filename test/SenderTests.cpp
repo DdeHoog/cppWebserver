a35a599869fb02d812b3de2657152e3eec5c86f4
Damian <Damian190085@gmail.com>
2020-09-16 15:38:01 +0200

    simplified html file to be able to run test logic firstly

--- test/SenderTests.cpp
+++ test/SenderTests.cpp
@@ -4,6 +4,13 @@
 TEST(SenderTests, Sendtest) {
    
     EXPECT_EQ (Sender::Send(std::string "host", std::string "/html/index", std::string "type", std::string "version", std::string "errorcode", std::string "statusmessage"),
-     "version errorcode statusmessage\nContent-Type: type\nContent-Length: 777\n"); //class::method
-    //change params with values
+     "version errorcode statusmessage\nContent-Type: type\nContent-Length: 777\n<html>
+    <head>
+    <title>Home</title>
+    </head>
+    <body>
+    <h1>Home page</h1>
+    </body>
+    </html>"); 
+    
 }
